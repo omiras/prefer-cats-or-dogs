e3dceecfc213b1182e36d26052e56f401b4e4aaf
omiras <oscar.miras@gmail.com>
2023-11-09 12:49:24 +0100

    done

--- app.js
+++ app.js
@@ -8,8 +8,20 @@ createApp({
   setup() {
     const favoriteAnimal = ref("");
 
+    // Computed property para verificar si el animal favorito es un gato
+    const isFavoriteCat = computed(() => {
+      return favoriteAnimal.value === "gatos";
+    });
+
+    // Computed property para verificar si el animal favorito es un perro
+    const isFavoriteDog = computed(() => {
+      return favoriteAnimal.value === "perros";
+    });
+
     return {
       favoriteAnimal,
+      isFavoriteCat,
+      isFavoriteDog,
     };
   },
 }).mount("#app");
